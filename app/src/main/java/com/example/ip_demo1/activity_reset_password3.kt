661c95cd1d45ee75416921a8c37f8ac9d311c232
Titusz <titusz.boros@student.upt.ro>
2024-03-15 22:44:41 +0200

    added more reset password screens

---
+++ app/src/main/java/com/example/ip_demo1/activity_reset_password3.kt
@@ -0,0 +1,11 @@
+package com.example.ip_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_reset_password3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password3)
+    }
+}
