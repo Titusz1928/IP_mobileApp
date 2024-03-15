3a0e67fb1f384ba8b38c55cfe387bb677a8530b1
Titusz <titusz.boros@student.upt.ro>
2024-03-15 20:08:00 +0200

    Bottom Navigation Bar

---
+++ app/src/main/java/com/example/ip_demo1/Menu_Activity.kt
@@ -0,0 +1,41 @@
+package com.example.ip_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Menu_Activity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView:BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.home->{
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.profile->{
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                R.id.settings->{
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+                else-> false
+            }
+        }
+
+        replaceFragment(HomeFragment())
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
+    }
+}
