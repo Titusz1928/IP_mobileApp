59fbd7b16a870ca50b8fb9813992eb21d8c94e29
Titusz <titusz.boros@student.upt.ro>
2024-03-17 00:42:01 +0200

    Version1 full fr

---
+++ app/src/main/java/com/example/ip_demo1/recommendationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ip_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recommendationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommendation)
+    }
+}
