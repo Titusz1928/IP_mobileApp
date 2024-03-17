4e59955c9520899bf925219cb0fb86d87a0715d4
Titusz <titusz.boros@student.upt.ro>
2024-03-17 23:59:37 +0200

    test_chats_step2

--- app/src/main/java/com/example/ip_demo1/s_chat1Activity.kt
+++ app/src/main/java/com/example/ip_demo1/s_chat1Activity.kt
@@ -1,11 +1,22 @@
 package com.example.ip_demo1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class s_chat1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schat1)
+
+        val back=findViewById<FloatingActionButton>(R.id.btBack_to_chat)
+
+        back.setOnClickListener{
+            val intent = Intent(this, Menu_Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
