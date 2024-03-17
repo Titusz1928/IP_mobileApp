de0e740a1d9279f8682e8024ac7cb959f9041e13
Titusz <titusz.boros@student.upt.ro>
2024-03-17 13:54:49 +0200

    Version1 demo ready

--- app/src/main/java/com/example/ip_demo1/SettingsFragment.kt
+++ app/src/main/java/com/example/ip_demo1/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.ip_demo1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class SettingsFragment : Fragment() {
@@ -14,8 +16,18 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val buttonLogout = view.findViewById<Button>(R.id.button_logout)
+        buttonLogout.setOnClickListener {
+            // Navigate to MainActivity
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 
