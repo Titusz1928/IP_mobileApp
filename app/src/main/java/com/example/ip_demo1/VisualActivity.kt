f98d5bc5d2f39234b219dd34e635708519ec99d1
Titusz <titusz.boros@student.upt.ro>
2024-03-17 21:10:52 +0200

    test_start_visualizer

---
+++ app/src/main/java/com/example/ip_demo1/VisualActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ip_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class VisualActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visual)
+
+        val options = arrayOf("Tensiunea arterială", "Puls", "T. corporală","Greutate","Glicernie","Lumină","T. ambientală","Umiditate","Proximitate")
+
+        val spinner=findViewById<Spinner>(R.id.spinner)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Handle item selection
+                val selectedItem = options[position]
+                // Do something with the selected item
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+
+    }
+}
