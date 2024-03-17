714fbc8ae2af0976f2dd75a19e276ff13f829102
Titusz <titusz.boros@student.upt.ro>
2024-03-17 22:32:05 +0200

    test_chats_step1

---
+++ app/src/main/java/com/example/ip_demo1/s_chat1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.ip_demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class s_chat1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schat1)
+    }
+}
